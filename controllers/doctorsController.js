c62397655d7fe5df46c1da578dba352d6e37cf91
Leonardo-Rosales <leonardorosaleshuerta@gmail.com>
2025-01-20 17:25:38 +0100

    ADD show doctor

--- controllers/doctorsController.js
+++ controllers/doctorsController.js
@@ -8,6 +8,22 @@ function index(_, res) {
   });
 }
 
+function show(req, res) {
+
+  const id = parseInt(req.params.id)
+  const sql = `SELECT * FROM doctors WHERE id = ?`
+  connection.query(sql, [id], (err, results) => {
+    if (err) return res.status(500).json({ message: err.message })
+    if (results.length === 0)
+      return res.status(404).json({
+        error: 'Not Found',
+        message: 'Doctor not found',
+      })
+    res.json(results)
+  })
+}
+
+
 function storeReview(req, res) {
   const dataObj = req.body;
   const doctor_id = req.params.id;
@@ -58,4 +74,4 @@ function storeDoctors(req, res) {
   );
 }
 
-module.exports = { index, storeReview, storeDoctors };
+module.exports = { index, show, storeReview, storeDoctors };
