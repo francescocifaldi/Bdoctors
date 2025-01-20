f428740efeb104f48f13dddbb18271ce96f87314
GabrieleTuzzo <gabriele.tuzzo@gmail.com>
2025-01-20 17:00:01 +0100

    ADD store review

--- controllers/doctorsController.js
+++ controllers/doctorsController.js
@@ -8,4 +8,29 @@ function index(_, res) {
     });
 }
 
-module.exports = { index };
+function storeReview(req, res) {
+    const dataObj = req.body;
+    const doctor_id = req.params.id;
+
+    console.log(dataObj, doctor_id);
+    const sql = `INSERT INTO reviews (review, vote, doctor_id, first_name, last_name, email)
+                VALUES (?, ?, ${doctor_id}, ?, ?, ?)`;
+    connection.query(
+        sql,
+        [
+            dataObj.review,
+            dataObj.vote,
+            dataObj.first_name,
+            dataObj.last_name,
+            dataObj.email,
+        ],
+        (err, response) => {
+            if (err) {
+                res.status(500).json({ message: err.message });
+            }
+            res.status(201).send();
+        }
+    );
+}
+
+module.exports = { index, storeReview };
