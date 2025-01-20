3fbaf244e39c631686c1aac6735c2840dc62427f
Leonardo-Rosales <leonardorosaleshuerta@gmail.com>
2025-01-20 16:07:54 +0100

    ADD index show

--- controllers/doctorsController.js
+++ controllers/doctorsController.js
@@ -1,11 +1,26 @@
-const connection = require ('../data/db')
+const connection = require('../data/db')
 
 function index(req, res) {
     const sql = `SELECT * FROM doctors`
     connection.query(sql, (err, doctors) => {
         if (err) return res.status(500).json({ message: err.message })
-        res.json({ doctors})
+        res.json({ doctors })
     })
 }
 
-module.exports = {index}
+function show(req, res) {
+
+    const id = parseInt(req.params.id)
+    const sql = `SELECT * FROM doctors WHERE id = ?`
+    connection.query(sql, [id], (err, results) => {
+        if (err) return res.status(500).json({ message: err.message })
+        if (results.length === 0)
+            return res.status(404).json({
+                error: 'Not Found',
+                message: 'Doctor not found',
+            })
+        res.json(results)
+    })
+}
+
+module.exports = { index, show }
--- routers/doctorsRouter.js
+++ routers/doctorsRouter.js
@@ -1,7 +1,10 @@
-const express = require ('express')
+const express = require('express')
 const router = express.Router()
 const doctorsController = require('../controllers/doctorsController')
 
 router.get('/', doctorsController.index)
+
+///show
+router.get('/:id', doctorsController.show)
 
 module.exports = router
