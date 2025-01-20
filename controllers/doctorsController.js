cd6e6c187b883b33874921707f64564418696dec
GabrieleTuzzo <gabriele.tuzzo@gmail.com>
2025-01-20 15:35:40 +0100

    minor changes

--- controllers/doctorsController.js
+++ controllers/doctorsController.js
@@ -1,11 +1,11 @@
-const connection = require ('../data/db')
+const connection = require('../data/db');
 
-function index(req, res) {
-    const sql = `SELECT * FROM doctors`
+function index(_, res) {
+    const sql = `SELECT * FROM doctors`;
     connection.query(sql, (err, doctors) => {
-        if (err) return res.status(500).json({ message: err.message })
-        res.json({ doctors})
-    })
+        if (err) return res.status(500).json({ message: err.message });
+        res.json({ doctors });
+    });
 }
 
-module.exports = {index}
+module.exports = { index };
