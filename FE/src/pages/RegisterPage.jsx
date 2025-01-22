4df7ec41753602e8ab8e635a905096ada7b685c2
Damiano-Mariscoli <damiano.mariscoli2@gmail.com>
2025-01-22 16:09:51 +0100

    ADD: form register doctors

--- FE/src/pages/RegisterPage.jsx
+++ FE/src/pages/RegisterPage.jsx
@@ -23,7 +23,7 @@ export default function RegisterPage() {
         setFormData(initialFormData);
       })
       .catch((err) => {
-        console.err(err);
+        console.error(err);
       });
   }
   function onFormChange(e) {
@@ -39,20 +39,92 @@ export default function RegisterPage() {
     <>
       <form onSubmit={storeDoctor}>
         <div className="mb-3">
-          <label htmlFor="name" className="form-label">
+          <label htmlFor="first_name" className="form-label">
             Nome
           </label>
           <input
             type="text"
             className="form-control"
-            id="name"
-            name="name"
-            value={formData.name}
+            id="first_name"
+            name="first_name"
+            value={formData.first_name}
             placeholder="Nome"
             onChange={onFormChange}
           />
         </div>
-        <button type="submit">submit</button>
+        <div className="mb-3">
+          <label htmlFor="last_name" className="form-label">
+            Cognome
+          </label>
+          <input
+            type="text"
+            className="form-control"
+            id="last_name"
+            name="last_name"
+            value={formData.last_name}
+            placeholder="Cognome"
+            onChange={onFormChange}
+          />
+        </div>
+        <div className="mb-3">
+          <label htmlFor="address" className="form-label">
+            Indirizzo
+          </label>
+          <input
+            type="text"
+            className="form-control"
+            id="address"
+            name="address"
+            value={formData.address}
+            placeholder="Indirizzo"
+            onChange={onFormChange}
+          />
+        </div>
+        <div className="mb-3">
+          <label htmlFor="email" className="form-label">
+            Email
+          </label>
+          <input
+            type="text"
+            className="form-control"
+            id="email"
+            name="email"
+            value={formData.email}
+            placeholder="email"
+            onChange={onFormChange}
+          />
+        </div>
+        <div className="mb-3">
+          <label htmlFor="phone" className="form-label">
+            Telefono
+          </label>
+          <input
+            type="text"
+            className="form-control"
+            id="phone"
+            name="phone"
+            value={formData.phone}
+            placeholder="Telefono"
+            onChange={onFormChange}
+          />
+        </div>
+        <div className="mb-3">
+          <label htmlFor="spec" className="form-label">
+            Specializzazione
+          </label>
+          <input
+            type="text"
+            className="form-control"
+            id="spec"
+            name="spec"
+            value={formData.spec}
+            placeholder="Specializzazione"
+            onChange={onFormChange}
+          />
+        </div>
+        <button type="submit" className="btn btn-primary">
+          submit
+        </button>
       </form>
     </>
   );
