a3901f9345298f9b3a964e9888151f41ba9cc359
GabrieleTuzzo <gabriele.tuzzo@gmail.com>
2025-01-22 13:20:56 +0100

    ADD homepage filter

--- FE/src/pages/HomePage.jsx
+++ FE/src/pages/HomePage.jsx
@@ -6,32 +6,56 @@ import axios from 'axios';
 
 export default function HomePage() {
     const [doctors, setDoctors] = useState([]);
+    const [specialization, setSpecialization] = useState([]);
+
     function fetchDoctors() {
         axios
             .get(`${import.meta.env.VITE_ENV_URI}/api/doctors`)
             .then((res) => {
                 setDoctors(res.data.doctors);
+                fillSpecializationArray();
             })
             .catch((err) => {
                 console.error(err);
             });
     }
 
+    function fillSpecializationArray() {
+        let specializationArray = [];
+
+        specializationArray = doctors.map((doctor) => {
+            if (!specializationArray.includes(doctor.spec))
+                specializationArray.push(doctor.spec);
+            setSpecialization(specializationArray);
+        });
+    }
+
     useEffect(() => {
         fetchDoctors();
     }, []);
 
-    console.log(doctors);
+    // console.log(doctors);
+    console.log(specialization);
 
     return (
-        <Row>
-            {doctors.map((doctor, i) => (
-                <Col key={i}>
-                    <Link to={`/doctor/${doctor.id}`}>
-                        <DoctorCard doctor={doctor}></DoctorCard>
-                    </Link>
-                </Col>
-            ))}
-        </Row>
+        <>
+            <label htmlFor="specialization">Filter by specialization</label>
+            <select id="specialization">
+                {specialization.map((spec, i) => (
+                    <option key={i} value={spec}>
+                        {spec}
+                    </option>
+                ))}
+            </select>
+            <Row>
+                {doctors.map((doctor) => (
+                    <Col key={doctor.id}>
+                        <Link to={`/doctor/${doctor.id}`}>
+                            <DoctorCard doctor={doctor}></DoctorCard>
+                        </Link>
+                    </Col>
+                ))}
+            </Row>
+        </>
     );
 }
