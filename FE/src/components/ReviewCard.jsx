2539abdf58b5bcba12576dabb1176b7e1612fff6
Leonardo-Rosales <leonardorosaleshuerta@gmail.com>
2025-01-22 20:15:27 +0100

    ADD stars-vote (fontawsome)

--- FE/src/components/ReviewCard.jsx
+++ FE/src/components/ReviewCard.jsx
@@ -1,13 +1,37 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import { faStar as faStarSolid } from '@fortawesome/free-solid-svg-icons'
+import { faStar as faStarRegular } from '@fortawesome/free-regular-svg-icons'
+
+
 export default function ReviewCard({ reviews }) {
 
     const { first_name, last_name, vote, email, review } = reviews
 
+    const stars = []
+
+    for (let i = 0; i < 5; i++) {
+        if (i < vote) {
+            stars.push(faStarSolid)
+        } else {
+            stars.push(faStarRegular)
+        }
+
+    }
+
     return (
         <div>
-            <h3>{first_name} {last_name}</h3>
-            <p>{vote}</p>
-            <p>{email}</p>
-            <p>{review}</p>
+            <p>Nome: {first_name}</p>
+            <p>Cognome: {last_name}</p>
+            <p>Email: {email}</p>
+            <p>Recensione: {review}</p>
+            <p>Voto:
+                <span>
+                    {stars.map((star, i) => (
+                        <FontAwesomeIcon key={i} icon={star} />
+                    ))}
+                </span>
+            </p>
+
         </div>
     )
 }
