d097f250dd73007ee2422b4b9926cc6a3e35ca0c
GabrieleTuzzo <gabriele.tuzzo@gmail.com>
2025-01-22 11:07:28 +0100

    ADD project basic pages and app.jsx

--- FE/src/App.jsx
+++ FE/src/App.jsx
@@ -1,10 +1,18 @@
 import './App.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import { Route, Routes } from 'react-router';
+import HomePage from './pages/HomePage';
 
 function App() {
     return (
         <>
-            <h1>Hello World</h1>
+            <Routes>
+                <Route index element={<HomePage />}></Route>
+                <Route path="/search" element={<SearchPage />}></Route>
+                <Route path="/doctor">
+                    <Route path=":id" element={<DetailPage />}></Route>
+                    <Route path="/register" element={<RegisterPage />}></Route>
+                </Route>
+            </Routes>
         </>
     );
 }
