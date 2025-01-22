ddd0c30f4e5730b8c8a8c77c26c36fd496eb4806
GabrieleTuzzo <gabriele.tuzzo@gmail.com>
2025-01-22 11:23:42 +0100

    ADD bootstrap and react-bootstrap dependencies

--- FE/src/App.jsx
+++ FE/src/App.jsx
@@ -1,19 +1,31 @@
 import './App.css';
-import { Route, Routes } from 'react-router';
+import { Route, Routes, BrowserRouter } from 'react-router';
+import 'bootstrap/dist/css/bootstrap.min.css';
+// Layout Import
+import DefaultLayout from './layouts/DefaultLayout';
+// Pages Import
 import HomePage from './pages/HomePage';
+import SearchPage from './pages/SearchPage';
+import DetailPage from './pages/DetailPage';
+import RegisterPage from './pages/RegisterPage';
 
 function App() {
     return (
-        <>
+        <BrowserRouter>
             <Routes>
-                <Route index element={<HomePage />}></Route>
-                <Route path="/search" element={<SearchPage />}></Route>
-                <Route path="/doctor">
-                    <Route path=":id" element={<DetailPage />}></Route>
-                    <Route path="/register" element={<RegisterPage />}></Route>
+                <Route element={<DefaultLayout />}>
+                    <Route index element={<HomePage />}></Route>
+                    <Route path="/search" element={<SearchPage />}></Route>
+                    <Route path="/doctor">
+                        <Route path=":id" element={<DetailPage />}></Route>
+                        <Route
+                            path="register"
+                            element={<RegisterPage />}
+                        ></Route>
+                    </Route>
                 </Route>
             </Routes>
-        </>
+        </BrowserRouter>
     );
 }
 
