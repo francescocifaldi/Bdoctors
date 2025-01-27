daf395dc573ad78714839d0d32affdb67dd2b87a
GabrieleTuzzo <gabriele.tuzzo@gmail.com>
2025-01-27 15:55:43 +0100

    ADD styles

--- FE/src/pages/SearchPage.jsx
+++ FE/src/pages/SearchPage.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { useState, useEffect } from 'react';
 import { Link } from 'react-router';
 import DoctorCard from '../components/DoctorCard';
-import { Row, Col } from 'react-bootstrap';
+import { Row, Col, Form } from 'react-bootstrap';
 import axios from 'axios';
 import { useSearchParams } from 'react-router';
 
@@ -13,41 +13,38 @@ export default function SearchPage() {
 
     const query = searchParams.get('spec');
 
-
     useEffect(() => {
-
-
         const params = { spec: query };
-
 
         if (searchQuery) {
             params.search = searchQuery;
         }
 
-
-        axios.get(`${import.meta.env.VITE_ENV_URI}/api/doctors/search`, { params })
+        axios
+            .get(`${import.meta.env.VITE_ENV_URI}/api/doctors/search`, {
+                params,
+            })
             .then((response) => {
                 setDoctors(response.data.doctors);
             })
             .catch((err) => {
-                console.error("Errore nel recupero dei dottori:", err);
-            })
-
-
+                console.error('Errore nel recupero dei dottori:', err);
+            });
     }, [query, searchQuery]);
-
-
-
-
 
     return (
         <>
-
-            <input
-                type="text"
-                value={searchQuery}
-                onChange={(e) => setSearchQuery(e.target.value)}
-            />
+            <Row>
+                <Col md={4}>
+                    <Form.Control
+                        className="mb-3"
+                        type="text"
+                        placeholder="Cerca Dottore"
+                        value={searchQuery}
+                        onChange={(e) => setSearchQuery(e.target.value)}
+                    />
+                </Col>
+            </Row>
 
             <Row>
                 {doctors.map((doctor) => (
