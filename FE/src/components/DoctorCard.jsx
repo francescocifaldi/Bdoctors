b3ee07d4798e0a07956e4e7837bd611e7b004032
Damiano-Mariscoli <damiano.mariscoli2@gmail.com>
2025-01-29 15:51:10 +0100

    ADDED: home page style , card style, header style, public folder

--- FE/src/components/DoctorCard.jsx
+++ FE/src/components/DoctorCard.jsx
@@ -1,22 +1,20 @@
-import React from 'react';
-import Card from 'react-bootstrap/Card';
+import React from "react";
+import Card from "react-bootstrap/Card";
 
 export default function DoctorCard({ doctor }) {
-    const { last_name, first_name, spec } = doctor;
+  const { last_name, first_name, spec } = doctor;
 
-    return (
-        <Card className="h-100">
-            <Card.Body>
-                <Card.Title>{`${first_name} ${last_name}`}</Card.Title>
-                <Card.Subtitle className="mb-2 text-muted">
-                    {spec}
-                </Card.Subtitle>
-                <Card.Text>
-                    Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-                    Officiis eligendi doloremque, autem adipisci tenetur aut
-                    esse maxime ducimus consequatur consectetur.
-                </Card.Text>
-            </Card.Body>
-        </Card>
-    );
+  return (
+    <Card className="h-100 cardD">
+      <Card.Header className="p-0">
+        <Card.Img variant="top" src="./doctor.jpg" className="img-fluid" />
+      </Card.Header>
+      <Card.Body>
+        <Card.Title className="text-center">{`${first_name} ${last_name}`}</Card.Title>
+        <Card.Subtitle className="mb-2 text-muted text-center">
+          {spec}
+        </Card.Subtitle>
+      </Card.Body>
+    </Card>
+  );
 }
