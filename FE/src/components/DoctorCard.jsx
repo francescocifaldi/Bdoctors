efa9e03c7fffbed016ac3afa3a49147a014fd0ad
francescocifaldi <francesco.cifaldi@gmail.com>
2025-02-01 12:31:27 +0100

    FIX doctorcard with img from db

--- FE/src/components/DoctorCard.jsx
+++ FE/src/components/DoctorCard.jsx
@@ -1,13 +1,22 @@
 import Card from "react-bootstrap/Card";
 
 export default function DoctorCard({ doctor }) {
-  const { last_name, first_name, spec } = doctor;
+  const { last_name, first_name, spec, image } = doctor;
 
   return (
     <div className="">
       <Card className="h-100 cardD">
         <Card.Header className="p-0">
-          <Card.Img variant="top" src="../doctor.jpg" className="img-fluid" />
+          <Card.Img
+            variant="top"
+            src={
+              image
+                ? `${import.meta.env.VITE_ENV_URI}/img/${doctor.image}`
+                : `${import.meta.env.VITE_ENV_URI}/img/doctorplaceholder.png`
+            }
+            alt={`${doctor.first_name} ${doctor.last_name} profile`}
+            className="img-fluid"
+          />
         </Card.Header>
         <Card.Body>
           <Card.Title className="text-center">{`${first_name} ${last_name}`}</Card.Title>
