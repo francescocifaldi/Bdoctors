d9c7b786fc2a820a45153c4b43593130af37e72b
francescocifaldi <francesco.cifaldi@gmail.com>
2025-02-02 16:52:36 +0100

    ADD layout changes on detailpage

--- FE/src/components/ReviewCard.jsx
+++ FE/src/components/ReviewCard.jsx
@@ -1,37 +1,42 @@
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
-import { faStar as faStarSolid } from '@fortawesome/free-solid-svg-icons'
-import { faStar as faStarRegular } from '@fortawesome/free-regular-svg-icons'
-
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faStar as faStarSolid } from "@fortawesome/free-solid-svg-icons";
+import { faStar as faStarRegular } from "@fortawesome/free-regular-svg-icons";
 
 export default function ReviewCard({ reviews }) {
+  const { first_name, last_name, vote, email, review } = reviews;
 
-    const { first_name, last_name, vote, email, review } = reviews
+  const stars = [];
 
-    const stars = []
-
-    for (let i = 0; i < 5; i++) {
-        if (i < vote) {
-            stars.push(faStarSolid)
-        } else {
-            stars.push(faStarRegular)
-        }
-
+  for (let i = 0; i < 5; i++) {
+    if (i < vote) {
+      stars.push(faStarSolid);
+    } else {
+      stars.push(faStarRegular);
     }
+  }
 
-    return (
-        <div>
-            <p><strong>Nome:</strong> {first_name}</p>
-            <p><strong>Cognome:</strong> {last_name}</p>
-            <p><strong>Email:</strong> {email}</p>
-            <p><strong>Recensione:</strong> {review}</p>
-            <p><strong>Voto:</strong>
-                <span>
-                    {stars.map((star, i) => (
-                        <FontAwesomeIcon key={i} icon={star} />
-                    ))}
-                </span>
-            </p>
-
-        </div>
-    )
+  return (
+    <div className="mt-2 border-top p-1">
+      <p>
+        <strong>Nome:</strong> {first_name}
+      </p>
+      <p>
+        <strong>Cognome:</strong> {last_name}
+      </p>
+      <p>
+        <strong>Email:</strong> {email}
+      </p>
+      <p>
+        <strong>Recensione:</strong> {review}
+      </p>
+      <p>
+        <strong>Voto:</strong>
+        <span>
+          {stars.map((star, i) => (
+            <FontAwesomeIcon key={i} icon={star} />
+          ))}
+        </span>
+      </p>
+    </div>
+  );
 }
--- FE/src/pages/DetailPage.jsx
+++ FE/src/pages/DetailPage.jsx
@@ -8,7 +8,7 @@ import { faStar as faStarSolid } from "@fortawesome/free-solid-svg-icons";
 import { faStar as faStarRegular } from "@fortawesome/free-regular-svg-icons";
 import { useContext } from "react";
 import GlobalContext from "../../contexts/globalContext";
-import { Button, Modal } from "react-bootstrap";
+import { Button, Modal, Row, Col } from "react-bootstrap";
 import { useNavigate } from "react-router";
 import ContactForm from "../components/ContactForm";
 import NotFound from "../components/NotFound";
@@ -67,101 +67,124 @@ export default function DetailPage() {
 
   return doctor ? (
     <section className="container">
+      <Button
+        className="w-100 mb-1"
+        variant="secondary"
+        onClick={() => navigate(-1)}
+        type="button"
+      >
+        Torna indietro
+      </Button>
       <div className="doctor-info">
-        <Button
-          className="w-100 mb-1"
-          variant="secondary"
-          onClick={() => navigate(-1)}
-          type="button"
-        >
-          Torna indietro
-        </Button>
-        <div className="doctor-card">
-          <figure>
-            <img
-              src={
-                doctor.image
-                  ? `${import.meta.env.VITE_ENV_URI}/uploads/img/${
-                      doctor.image
-                    }`
-                  : `${import.meta.env.VITE_ENV_URI}/img/doctorplaceholder.png`
-              }
-              alt={`${doctor.first_name} ${doctor.last_name} profile`}
-            />
-          </figure>
+        <div className="container d-flex justify-content-center flex-column align-items-center py-5">
           <h1>
             {doctor.first_name} {doctor.last_name}
           </h1>
           <p>
-            <strong>Città:</strong> {doctor.city}
-          </p>
-          <p>
-            <strong>Indirizzo:</strong> {doctor.address}
-          </p>
-          <p>
-            <strong>Email:</strong> {doctor.email}
-          </p>
-          <p>
-            <strong>Telefono:</strong> {doctor.phone}
-          </p>
-          <p>
-            <strong>Specializzazione:</strong> {doctor.spec}
-          </p>
-          <p>
-            <strong>Descrizione: </strong>
-            {doctor.description || (
-              <i>Nessuna descrizione inserita dal medico</i>
+            {doctor.reviews.length ? (
+              <>
+                <span>
+                  {stars.map((star, i) => (
+                    <FontAwesomeIcon key={i} icon={star} />
+                  ))}
+                </span>
+                <span>({doctor.avg_vote})</span>
+                <br />
+                <i>{`in base a ${doctor.reviews.length} recensioni`}</i>
+              </>
+            ) : (
+              <i>Ancora nessuna valutazione</i>
             )}
           </p>
-          <p>
+          {/* Bottone per aprire il Modale di Contatto */}
+          <Button
+            variant="success"
+            onClick={() => setShowContact(true)}
+            className="ms-2"
+          >
+            Contatta il medico
+          </Button>
+        </div>
+
+        <Row>
+          <Col>
+            <figure>
+              <img
+                src={
+                  doctor.image
+                    ? `${import.meta.env.VITE_ENV_URI}/uploads/img/${
+                        doctor.image
+                      }`
+                    : `${
+                        import.meta.env.VITE_ENV_URI
+                      }/img/doctorplaceholder.png`
+                }
+                alt={`${doctor.first_name} ${doctor.last_name} profile`}
+                style={{ width: "100%", objectFit: "contain" }}
+              />
+            </figure>
+
+            <p>
+              <strong>Città:</strong> {doctor.city}
+            </p>
+            <p>
+              <strong>Indirizzo:</strong> {doctor.address}
+            </p>
+            <p>
+              <strong>Email:</strong> {doctor.email}
+            </p>
+            <p>
+              <strong>Telefono:</strong> {doctor.phone}
+            </p>
+            <p>
+              <strong>Specializzazione:</strong> {doctor.spec}
+            </p>
+          </Col>
+
+          <Col>
             <strong>CV: </strong>
             {doctor.cv ? (
               <iframe
                 src={`${import.meta.env.VITE_ENV_URI}/uploads/cv/${doctor.cv}`}
+                height="100%"
                 width="100%"
-                height="600px"
                 style={{ border: "none" }}
                 title="PDF Viewer"
               ></iframe>
             ) : (
               <i>Nessun CV caricato</i>
             )}
-          </p>
-          <p>
-            <strong>Valutazione: </strong>
-            {doctor.reviews.length ? (
-              <>
-                <span>{doctor.avg_vote}</span>
-                <span>
-                  {stars.map((star, i) => (
-                    <FontAwesomeIcon key={i} icon={star} />
-                  ))}
-                </span>
-              </>
-            ) : (
-              <i>Ancora nessuna valutazione</i>
-            )}
-          </p>
+          </Col>
+        </Row>
+        <p>
+          <strong>Descrizione: </strong>
+          {doctor.description || <i>Nessuna descrizione inserita dal medico</i>}
+        </p>
+      </div>
+
+      <div className="doctor-info">
+        <div className="container d-flex justify-content-between">
+          {doctor.reviews.length ? (
+            <h2>Recensioni</h2>
+          ) : (
+            <h2>Ancora nessuna recensione</h2>
+          )}
+
+          {/* Bottone per aprire il Modale di Recensione */}
+          <Button variant="primary" onClick={() => setShowReview(true)}>
+            Aggiungi recensione
+          </Button>
         </div>
 
-        {doctor.reviews.length ? (
-          <div className="reviews-list">
-            <h2>Recensioni</h2>
+        {doctor.reviews.length && (
+          <div>
             <ul>
               {doctor.reviews.map((review) => (
                 <ReviewCard reviews={review} key={review.id} />
               ))}
             </ul>
           </div>
-        ) : (
-          <div>Nessuna recensione</div>
         )}
-      </div>
-      <div>
-        {/* Bottone per aprire il Modale di Recensione */}
-        <Button variant="primary" onClick={() => setShowReview(true)}>
-          Aggiungi recensione
-        </Button>
 
         {/* Modale per la Recensione */}
         <Modal show={showReview} onHide={() => setShowReview(false)}>
@@ -176,15 +199,6 @@ export default function DetailPage() {
             />
           </Modal.Body>
         </Modal>
-
-        {/* Bottone per aprire il Modale di Contatto */}
-        <Button
-          variant="success"
-          onClick={() => setShowContact(true)}
-          className="ms-2"
-        >
-          Contatta il medico
-        </Button>
 
         {/* Modale per il Contatto */}
         <Modal show={showContact} onHide={() => setShowContact(false)}>
