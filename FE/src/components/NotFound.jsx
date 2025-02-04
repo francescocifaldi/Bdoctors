69b9e78ca2e015a025f11459362bda589dafc797
francescocifaldi <francesco.cifaldi@gmail.com>
2025-02-04 09:55:09 +0100

    ADD minor changes 404

--- FE/src/components/NotFound.jsx
+++ FE/src/components/NotFound.jsx
@@ -3,7 +3,6 @@ export default function NotFound() {
     <div className="d-flex flex-column container justify-content-center align-items-center h-100 text-light mb-5">
       <h1>Error 404</h1>
       <h2>Pagina non trovata!</h2>
-      <p>La ricerca non ha prodotto risultati.</p>
     </div>
   );
 }
